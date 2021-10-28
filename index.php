af66d424998a376f06f6f0579be8bc86bece538f
Hugo Di Paolo <73029436+Cliffy57@users.noreply.github.com>
2021-10-28 20:01:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+
+<?php
+
+session_start();
+$_SESSION['url'] ='index.php';
+require_once('connexion.php');
+// si l'application utilise exclusivement les requêtes préparées,
+// alors aucune injection SQL n'est possible
+// donc toutes les $_POST, $_GET, etc insérées dans execute() ou bindValue/bindPrepare
+// après avoir fait une requete préparée avec prepare() : aucun risque d'injection.
+$req = 'select idsujet, count(*) as nb from sujet group by idsujet';
+$result = $objPdo->prepare($req);
+$result->execute();
+$sujets=array();
+foreach($result as $row)
+{ $sujets[$row['idsujet']]=$row['nb']; }
+$req = 'select * from sujet';
+$result = $objPdo->prepare($req);
+$result->execute();
+$ch = '<table border="1">';
+$ch .='<tr><th>Titre</th><th>Date</th><th>Redacteur</th><th>Sujet</th></tr>';
+foreach($result as $row)
+{
+$ch .='<tr>';
+$ch .='<td>' .$row['titresujet'] . '</td>';
+$ch .='<td>' .$row['datesujet'] . '</td>';
+$ch .='<td>' .$row['idredacteur'] . '</td>';
+$ch .='<td>' .$row['textesujet'] . '</td>';
+if (array_key_exists($row['idsujet'],$sujets))
+{
+$lib = 'Visualiser';
+if ($sujets[$row['idsujet']] > 1)
+{ $lib .= 's'; }
+$ch .='<td><a href="visualiserSujet.php?id=' .urlencode($row['idsujet']) .'">' .$lib .'</a></td>';
+}
+else
+{
+$ch .='<td><a href="editSujet.php?a&id=' .urlencode($row['idsujet']) .'">Ajouter Sujet</a></td>';
+}
+}
+$ch .= '</table>';
+unset($result);
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Site de Blog</title>
+<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<h1>Page d'accueil</h1>
+<h2>Liste des sujets</h2>
+<button type="button"class="authent" onclick="document.location.href='connect.php'">S'authentifier</button>
+<button type="button"class="register"onclick="document.location.href='register.php'">S'inscrire</button>
+<?php echo($ch); ?>
+</body>
+</html>
---
+++ visualiserSujet.php
@@ -0,0 +1,34 @@
+<?php session_start(); ?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sujet</title>
+<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<?php
+include 'sujet.class.php';
+$sujets = unserialize(file_get_contents('data/sujets'));
+if (isset($_GET['ref']))
+{	$unsujet = $sujets[$_GET['ref']];	}
+else
+{	header("location:index.php");		}
+?>
+<h1>Voici le sujet:</h1>
+<dl>
+  <dt>Référence :</dt>
+  <dd><?php echo $_GET['ref'] ?></dd>
+  <dt>Designation :</dt>
+  <dd><?php echo $unarticle->getDesignation() ?></dd>
+  <dt>Prix unitaire :</dt>
+  <dd><?php echo $unarticle->getPrix() ?></dd>
+</dl>
+<form name="achat" action="ajouter.php<?php echo '?ref='.$_GET['ref'] ?>" 
+	  method="post">
+  <label for="commentaire">Commentaire</label>
+  <input id="commentaire" name="commentaire" type="text" />
+  <input type="submit" value="Ajouter un commentaire" /> 
+</form>
+<a href="commander.php">Annuler l'achat et retourner à la liste des articles</a>
+</body>
+</html>
