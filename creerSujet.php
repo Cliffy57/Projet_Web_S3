5406dacb3a922f475ff17e07d8ecb730f5c3faff
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2021-11-04 22:37:54 +0100

    creerSujet done

--- creerSujet.php
+++ creerSujet.php
@@ -1,32 +1,49 @@
 <?php
+session_start();
 require_once "config.php";
 // define variables and set to empty values
-$erreur=array();
-$valeur=array();
-$valeur["titre"]=""; $erreur["titre"]="";
-$valeur["texte"]=""; $erreur["texte"]="";
+$textesujetErr = $titresujetErr = "";
+$textesujet = $titresujet = "";
+
+echo ($_SESSION["id"]);
+echo (date("Y-m-d"));
 if (isset($_POST['submit'])) {
 
 
-if (!isset($_POST['titresujet']) or strlen(trim($_POST['titresujet']))==0)
-{ $erreur['titre'] = 'saisie obligatoire du titre'; }
-else { $valeur['titre'] = trim($_POST['titresujet']); }
-if (!isset($_POST['textesujet']) or strlen(trim($_POST['textesujet']))==0)
-{ $erreur['texte'] = 'saisie obligatoire du texte'; }
-else { $valeur['texte'] = trim($_POST['textesujet']); }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty(trim($_POST["titresujet"]))) {
+      $titresujetErr = "Precisez le titre de votre Sujet.";
+    } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $titresujet)) {
+      $titresujetErr = "Seul les lettres et les espaces sont toleres";
+    } else {
+      // Set parameters
+      $titresujet = trim($_POST["titresujet"]);
+    }
+  }
+  if (empty(trim($_POST["textesujet"]))) {
+    $textesujetErr = "Precisez le texte de votre Sujet.";
+  } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $textesujet)) {
+    $textesujetErr = "Seul les lettres et les espaces sont toleres";
+  } else {
+    // Set parameters
+    $textesujet = trim($_POST["textesujet"]);
+  }
+
 
   // Check input errors before inserting in database
-  if (count($erreur)==0){
+  if (empty($titresujetErr) && empty($textesujetErr)) {
 
     ////////////////////////////////
-    $insert_stmt = $objPdo->prepare('INSERT INTO sujet (idredacteur,titresujet,textesujet,datesujet) VALUES(:idredacteur,:titre,:texte,:date)');
+    $insert_stmt = $objPdo->prepare('INSERT INTO sujet (idredacteur,titresujet,textesujet,datesujet) VALUES(:idredacteur,:titresujet,:textesujet,:datesujet)');
     $insert_stmt->bindValue("idredacteur", $_SESSION['id'], PDO::PARAM_STR);
-    $insert_stmt->bindValue("titre", $titresujet, PDO::PARAM_STR);
-    $insert_stmt->bindValue("texte", $textesujet, PDO::PARAM_STR);
-    $insert_stmt->bindValue("currentDate",date("Y/m/d"), PDO::PARAM_STR);
+    $insert_stmt->bindValue("titresujet", $titresujet, PDO::PARAM_STR);
+    $insert_stmt->bindValue("textesujet", $textesujet, PDO::PARAM_STR);
+    $insert_stmt->bindValue("datesujet", date('Y-m-d H:i:s'), PDO::PARAM_STR);
     $insert_stmt->execute();
     header("Location: index.php");
     ////////////////////////////////
+  } else {
+    echo "Erreur lors de la creation du sujet !";
   }
 }
 
@@ -59,16 +76,16 @@ else { $valeur['texte'] = trim($_POST['textesujet']); }
     <h2>Creation d'un Sujet</h2>
     <p>Remplissez ce formulaire afin de creer votre compte.</p>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <div>
-                <label>Titre du sujet:</label>
-                <input type="text" name="titresujet" value='<?php echo $valeur["titre"];?>'></input>
-                <span class="erreur"><?php echo $erreur['titre'] ;?></span>
-            </div>
-            <div>
-                <label>Texte de votre sujet:</label>
-                <textarea name="textesujet" maxlength="900" value="<?php echo $valeur['texte'];?>"></textarea>
-                <span class="erreur"><?php echo $erreur['texte'] ;?></span>
-            </div>
+      <div>
+        <label>Titre du sujet:</label>
+        <input type="text" name="titresujet" value='<?php echo $titresujet; ?>'></input>
+        <span class="erreur"><?php echo $erreur['titre']; ?></span>
+      </div>
+      <div>
+        <label>Texte de votre sujet:</label>
+        <textarea name="textesujet" maxlength="900" value="<?php echo $textesujet; ?>"></textarea>
+        <span class="erreur"><?php echo $erreur['texte']; ?></span>
+      </div>
       <input type="submit" name="submit" value="Ajouter le Sujet">
     </form>
   </div>
