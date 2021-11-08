9551b2e2610ee1caef88766b4819ce04ca39427d
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2021-11-08 15:06:54 +0100

    p

--- supprimerrep.php
+++ supprimerrep.php
@@ -2,12 +2,12 @@
 
 require_once "config.php";
 include_once('visualiserSujet.php');
-print_r($_GET);
-print_r($_POST);
+// print_r($_GET);
+// print_r($_POST);
 
 if (isset($_POST['delete'])) {
-  echo('mouton');
-  echo($_GET['idrep']);
+
+  // echo($_GET['idrep']);
   $delete_stmt = $objPdo->prepare('DELETE FROM reponse WHERE reponse.idreponse=:idreponse');
   $delete_stmt->bindValue("idreponse",  $_GET['idrep'], PDO::PARAM_STR);
   $delete_stmt->execute();
